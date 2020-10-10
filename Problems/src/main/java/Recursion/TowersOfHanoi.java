993f04045982427ab54a04a674cf7fa06fcec233
Grigory Bushuev <grigorybushuev@hotmail.com>
2020-10-10 17:21:18 -0400

    Add TowersOfHanoi solution

--- Problems/src/main/java/Recursion/TowersOfHanoi.java
+++ Problems/src/main/java/Recursion/TowersOfHanoi.java
@@ -1,38 +1,69 @@
 package Recursion;
 
-import java.util.List;
+import java.util.*;
 
 public class TowersOfHanoi {
 
-    private class Move {
+    private static class Move {
         private final int index;
-        private final int source;
-        private final int destination;
+        private final String source;
+        private final String destination;
 
-        public Move(int index, int source, int destination){
+        public Move(final int index, String source, String destination){
             this.index = index;
             this.source = source;
             this.destination = destination;
         }
 
-        public int getIndex() {
-            return index;
-        }
-
-        public int getSource() {
-            return source;
-        }
-
-        public int getDestination() {
-            return destination;
+        @Override
+        public String toString() {
+            return "Move " +
+                    "disk=" + index +
+                    " from=" + source +
+                    " to=" + destination;
         }
     }
 
-    public static List<Move> getMoves(int n) {
-        return null;
+    private static class Peg
+    {
+        public final String name;
+        public final Stack<Integer> peg;
+        public Peg(String name){
+            this.name = name;
+            this.peg = new Stack<>();
+        }
+
+    }
+
+    private static Move getMoveFromTop(Peg start, Peg dest, int n) {
+        var top = start.peg.pop();
+        dest.peg.push(top);
+        return new Move(n, start.name, dest.name);
+    }
+
+    public static List<Move> getMoves(Peg start, Peg aux, Peg dest, int n) {
+        if (n == 0){
+            return new ArrayList<>();
+        }
+        var result = new ArrayList<Move>();
+        result.addAll(getMoves(start, dest, aux, n - 1));
+        result.add(getMoveFromTop(start, dest, n));
+        result.addAll(getMoves(aux, start, dest, n- 1));
+        return result;
     }
 
     public static void main(String[] args) {
+        var startTower = new Peg("start");
+        var auxTower = new Peg("aux");
+        var destTower = new Peg("dest");
 
+        final int height = 3;
+        for (var i = height - 1; i >= 0;  i--) {
+            startTower.peg.push(i);
+        }
+
+        for (var move : getMoves(startTower, auxTower, destTower, height)){
+            System.out.println(move);
+        }
     }
 }
