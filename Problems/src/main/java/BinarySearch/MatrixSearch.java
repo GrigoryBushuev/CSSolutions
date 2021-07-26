c7af82e7df784824cb48ab73a18f620b707a70b0
Grigory <GrigoryBushuev@hotmail.com>
2021-07-25 20:09:46 -0400

    Add search matrix

--- Problems/src/main/java/BinarySearch/MatrixSearch.java
+++ Problems/src/main/java/BinarySearch/MatrixSearch.java
@@ -1,31 +1,35 @@
 package BinarySearch;
 
-class Solution {
+class MatrixSearch {
     public boolean hasTarget(int[][] matrix, int startIndex, int target) {
         var lo = startIndex;
-        var hi = matrix.length - 1;
+        var hi = matrix[0].length - 1;
 
-        while (lo < hi) {
+        if (startIndex >  hi) {
+            return false;
+        }
+
+        while (lo <= hi) {
             var current = lo + ((hi - lo) / 2);
             if (matrix[startIndex][current] == target) {
                 return true;
             } else if (matrix[startIndex][current] > target) {
-                hi = current;
+                hi = current - 1;
             } else {
-                lo = current;
+                lo = current + 1;
             }
         }
 
         lo = startIndex;
-        hi = matrix[startIndex].length - 1;
-        while (lo < hi) {
+        hi = matrix.length - 1;
+        while (lo <= hi) {
             var current = lo + ((hi - lo) / 2);
             if (matrix[current][startIndex] == target) {
                 return true;
             } else if (matrix[current][startIndex] > target) {
-                hi = current;
+                hi = current - 1;
             } else {
-                lo = current;
+                lo = current + 1;
             }
         }
         return false;
@@ -33,14 +37,39 @@ class Solution {
 
     public boolean searchMatrix(int[][] matrix, int target) {
         //assert null
-        var m = matrix.length;
-        var n = matrix[0].length;
+        var m = matrix[0].length;
+        var n = matrix.length;
         if (m == 1 && n == 1) {
             return matrix[0][0] == target;
         }
         for (var i = 0; i < n; i++) {
             if (hasTarget(matrix, i, target)) {
                 return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean quickSearchMatrix(int[][] matrix, int target) {
+        //assert null
+        var m = matrix[0].length;
+        var n = matrix.length;
+        if (m == 1 && n == 1) {
+            return matrix[0][0] == target;
+        }
+
+        var colIndex = 0;
+        var rowIndex = n - 1; //3
+
+        while (rowIndex >= 0 && colIndex < m) {
+            if (matrix[rowIndex][colIndex] == target) {
+                return true;
+            }
+            if (matrix[rowIndex][colIndex] > target) {
+                rowIndex--;
+            }
+            if (rowIndex >= 0 && matrix[rowIndex][colIndex] < target) {
+                colIndex++;
             }
         }
         return false;
