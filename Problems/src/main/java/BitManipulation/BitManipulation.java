4887cc173b944bd6c3d69932e0be7258d01227fe
Grigory <GrigoryBushuev@hotmail.com>
2022-04-07 09:16:24 -0400

    reverse bits

--- Problems/src/main/java/BitManipulation/BitManipulation.java
+++ Problems/src/main/java/BitManipulation/BitManipulation.java
@@ -1,6 +1,32 @@
 package BitManipulation;
+import Math.*;
+
 
 public class BitManipulation {
+	public static String toBinaryString(int n) {
+		var buf = new char[32];
+		for (var i = 31; i >= 0; i--) {
+			buf[i] = (n & 1) == 1 ? '1' : '0';
+			n >>>= 1;
+		}
+		return new String(buf);
+	}
+
+	public static long parseLong(String binaryString, int radix) throws NumberFormatException {
+		//null check
+		//length check
+
+		//return Long.parseLong(binaryString, 2);
+		long result = 0;
+		var len = binaryString.length();
+		var exponent = 0;
+		for (var i = len - 1; i >= 0; i--){
+			var val = binaryString.charAt(i);
+			result += Character.getNumericValue(val) * Utils.binaryExponentiation(radix, exponent++);
+		}
+		return result;
+	}
+
 	public static int getBitsCount(int n) {
 		int result = 0;
 		while (n > 0) {
@@ -12,8 +38,43 @@ public class BitManipulation {
 		return result;
 	}
 
-	public static void main(String[] args){
-		System.out.println(getBitsCount(1));
+	public int reverseBits(int input) {
+		var result = 0;
+		for (var i = 31; i >=0; i--) {
+			if ((input & 1) == 1) {
+				result |= (1 << i);
+			}
+			input >>= 1;
+		}
+		return result;
+	}
+
+	public static int reverseBitsWithoutIterations(int input) {
+		var swappedAdjacentBits = swapAdjacentBits(input);
+		var swappedPairs = swapPairs(swappedAdjacentBits);
+		var swappedQuaters = swapQuaters(swappedPairs);
+		return swapEndianness(swappedQuaters);
+	}
+
+	public static int swapEndianness(int n) {
+		var firstByte = (n & 0x000000FF) << 24;
+		var secondByte = (n & 0x0000FF00) << 8;
+		var thirdByte = (n & 0x00FF0000) >>> 8;
+		var fourthByte = (n & 0xFF000000) >>> 24;
+
+		return firstByte | secondByte | thirdByte | fourthByte;
+	}
+
+	public static int swapAdjacentBits(int n) {
+		return ((n & 0xAAAAAAAA) >>> 1) | ((n & 0x55555555) << 1);
+	}
+
+	public static int swapPairs(int n) {
+		return ((n & 0xCCCCCCCC) >>> 2) | ((n & 0x33333333) << 2);
+	}
+
+	public static int swapQuaters(int n) {
+		return ((n & 0xF0F0F0F0) >>> 4) | ((n & 0x0F0F0F0F) << 4);
 	}
 }
 
--- Problems/src/test/java/BitManipulation/BitManipulationTest.java
+++ Problems/src/test/java/BitManipulation/BitManipulationTest.java
@@ -1,4 +1,43 @@
 package BitManipulation;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class BitManipulation {
+public class BitManipulationTest {
+    @Test
+    public void reverseBitsTest() {
+        var sut = new BitManipulation();
+        var result = sut.reverseBits(43261596);
+        Assert.assertEquals(964176192, result);
+    }
+
+    @Test
+    public void swapEndiannessTest() {
+        var sut = new BitManipulation();
+        var result = sut.swapEndianness(0x12345678);
+        Assert.assertEquals(0x78563412, result);
+    }
+
+    @Test
+    public void toBinaryStringTest() {
+        var sut = new BitManipulation();
+        var result = sut.toBinaryString(11);
+        Assert.assertEquals("00000000000000000000000000001011", result);
+    }
+
+    @Test
+    public void parseIntTest() {
+        var sut = new BitManipulation();
+//        var result = sut.parseLong("00000000000000000000000000001011", 2);
+//        Assert.assertEquals(11, result);
+
+        var result = sut.parseLong("10101010101010101010101010101010", 2);
+        Assert.assertEquals(0x00000000AAAAAAAA, result);
+    }
+
+    @Test
+    public void reverseBitsWithoutIterationsTest() {
+        var sut = new BitManipulation();
+        var result = sut.reverseBitsWithoutIterations(43261596);
+        Assert.assertEquals(964176192, result);
+    }
 }
