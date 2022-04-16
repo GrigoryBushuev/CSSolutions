5b0f94f789f36b9600a4d03901d769375e67e81f
Grigory <GrigoryBushuev@hotmail.com>
2022-04-16 14:27:37 -0400

    Sum strings

--- Problems/src/main/java/Math/Utils.java
+++ Problems/src/main/java/Math/Utils.java
@@ -1,6 +1,15 @@
 package Math;
 
 public class Utils {
+
+    /*2^17 = 2^8 * 2^8 * 2^1
+     *
+     * 1) result = a * 1 = a; a = a ^ 2; n = 8;
+     * 2) result = a; a = a ^ 2 * a ^ 2 = a ^ 4; n = 4;
+     * 3) result = a; a = a ^ 4 * a ^ 4 = a ^ 8; n = 2;
+     * 4) result = a; a = a ^ 8 * a ^ 8 = a ^ 16; n = 1;
+     * 5) return result * a; return a * a ^ 16;
+     */
     public static double binaryExponentiation(double a, int n) {
         if (n == 0) {
             return 1;
@@ -23,12 +32,27 @@ public class Utils {
         return result * a;
     }
 
-    /*2^17 = 2^8 * 2^8 * 2^1
-    *
-    * 1) result = a * 1 = a; a = a ^ 2; n = 8;
-    * 2) result = a; a = a ^ 2 * a ^ 2 = a ^ 4; n = 4;
-    * 3) result = a; a = a ^ 4 * a ^ 4 = a ^ 8; n = 2;
-    * 4) result = a; a = a ^ 8 * a ^ 8 = a ^ 16; n = 1;
-    * 5) return result * a; return a * a ^ 16;
-    */
+    public String addStrings(String a, String b) {
+        var sb = new StringBuilder();
+        var aPointer = a.length() - 1;
+        var bPointer = b.length() - 1;
+        var carriedValue = 0;
+        while (aPointer >= 0 || bPointer >= 0 || carriedValue > 0) {
+            var aValue = 0;
+            var bValue = 0;
+            if (aPointer >= 0) {
+                aValue = a.charAt(aPointer) - '0';
+                aPointer--;
+            }
+            if (bPointer >= 0) {
+                bValue = b.charAt(bPointer) - '0';
+                bPointer--;
+            }
+            var sum = aValue + bValue  + carriedValue;
+            var result = sum % 10;
+            carriedValue = sum / 10;
+            sb.append(result);
+        }
+        return sb.reverse().toString();
+    }
 }
--- Problems/src/test/java/Math/UtilsTest.java
+++ Problems/src/test/java/Math/UtilsTest.java
@@ -18,4 +18,19 @@ public class UtilsTest {
         result = sut.binaryExponentiation(2, -2);
         Assert.assertEquals(0.25, result, 0.0f);
     }
+
+    @Test
+    public void addStringsTest() {
+        var sut = new Utils();
+        var result = sut.addStrings("123", "456");
+        Assert.assertEquals("579", result);
+
+        result = sut.addStrings("146", "456");
+        Assert.assertEquals("602", result);
+        result = sut.addStrings("10146", "456");
+        Assert.assertEquals("10602", result);
+
+        result = sut.addStrings("9", "9");
+        Assert.assertEquals("18", result);
+    }
 }
