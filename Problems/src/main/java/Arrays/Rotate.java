d859fb8a5c6b1a3ee8cf3becbc424679ae7b097a
Grigory <GrigoryBushuev@hotmail.com>
2022-06-09 22:51:11 -0400

    Add rotate Image

--- Problems/src/main/java/Arrays/Rotate.java
+++ Problems/src/main/java/Arrays/Rotate.java
@@ -8,15 +8,19 @@ public class Rotate {
     }
 
     public void rotate(int[][] matrix) {
-        var start = 0;
-        var end = matrix.length - 1;
-        var size = matrix.length / 2;
+        var matrixSize = matrix.length - 1;
+        var layerSize = matrix.length / 2;
 
-        for (var cur = 0; cur < size; cur++) {
-            for (var i = start + cur; i < end - cur; i++) {
-                swap(matrix, cur, i,  i,end - cur);
-                swap(matrix, cur, i, end - i, cur);
-                swap(matrix, end - i, cur, end - cur, end - i);
+        for (var layer = 0; layer < layerSize; layer++) {
+            for (var i = layer; i < matrixSize - layer; i++) {
+                var topLeft = matrix[layer][i];
+                var topRight = matrix[i][matrixSize-layer];
+                var bottomLeft = matrix[matrixSize - i][layer];
+                var bottomRight = matrix[matrixSize - layer][matrixSize - i];
+                matrix[i][matrixSize-layer] = topLeft;//tr
+                matrix[matrixSize - layer][matrixSize - i] = topRight;//br
+                matrix[matrixSize - i][layer] = bottomRight;//bl
+                matrix[layer][i] = bottomLeft;
             }
         }
     }
