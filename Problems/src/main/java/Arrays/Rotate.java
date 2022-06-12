a57019ad7763b1a56699b1bcaa338519a8902f3d
Grigory <GrigoryBushuev@hotmail.com>
2022-06-11 20:08:00 -0400

    Add rotate Image

--- Problems/src/main/java/Arrays/Rotate.java
+++ Problems/src/main/java/Arrays/Rotate.java
@@ -1,12 +1,6 @@
 package Arrays;
 
 public class Rotate {
-    private static void swap(int[][] matrix, int i, int j, int k, int l) {
-        var temp = matrix[i][j];
-        matrix[i][j] = matrix[k][l];
-        matrix[k][l] = temp;
-    }
-
     public void rotate(int[][] matrix) {
         var matrixSize = matrix.length - 1;
         var layerSize = matrix.length / 2;
@@ -21,6 +15,27 @@ public class Rotate {
                 matrix[matrixSize - layer][matrixSize - i] = topRight;//br
                 matrix[matrixSize - i][layer] = bottomRight;//bl
                 matrix[layer][i] = bottomLeft;
+            }
+        }
+    }
+
+    private static void swap(int[][] matrix, int i, int j, int k, int l) {
+        var temp = matrix[i][j];
+        matrix[i][j] = matrix[k][l];
+        matrix[k][l] = temp;
+    }
+
+    public void rotateTransposeReflect(int[][] matrix) {
+        //transpose
+        for (var i = 0; i < matrix.length; i++) {
+            for (var j = i; j < matrix.length; j++) {
+                swap(matrix, i, j, j, i);
+            }
+        }
+        //Reflect
+        for (var i = 0; i < matrix.length / 2; i++) {
+            for (var j = 0; j < matrix.length; j++) {
+                swap(matrix, j, i, j,matrix.length - 1 - i);
             }
         }
     }
--- Problems/src/test/java/Arrays/RotateTest.java
+++ Problems/src/test/java/Arrays/RotateTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 public class RotateTest {
-
     @Test
     public void rotateTest() {
         var sut = new Rotate();
@@ -13,6 +12,26 @@ public class RotateTest {
                 { 9, 10, 11, 12 },
                 { 13, 14, 15, 16 }
         };
-        sut.rotate(matrix);
+        sut.rotateTransposeReflect(matrix);
+        assertThat(matrix)
+                .isNotEmpty()
+                .hasSameDimensionsAs(matrix)
+                .isDeepEqualTo(new int[][] { {13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4} });
+    }
+
+    @Test
+    public void rotateTransposeReflectTest() {
+        var sut = new Rotate();
+        int[][] matrix = {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 }
+        };
+        sut.rotateTransposeReflect(matrix);
+        assertThat(matrix)
+                .isNotEmpty()
+                .hasSameDimensionsAs(matrix)
+                .isDeepEqualTo(new int[][] { {13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4} });
     }
 }
