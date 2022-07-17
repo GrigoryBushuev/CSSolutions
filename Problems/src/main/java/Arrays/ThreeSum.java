c03244e6b92c023933a59c5aac1d8690ee8f121d
Grigory <GrigoryBushuev@hotmail.com>
2022-07-16 20:53:01 -0400

    Add 3Sum

--- Problems/src/main/java/Arrays/ThreeSum.java
+++ Problems/src/main/java/Arrays/ThreeSum.java
@@ -1,6 +1,7 @@
 package Arrays;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 /*
 * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
@@ -22,61 +23,92 @@ Constraints:
     -105 <= nums[i] <= 105
 */
 public class ThreeSum {
-
-    private static Integer[] sort3Values(int a, int b, int c) {
-        Integer first, second, third = null;
-        if (a < b) {
-            if (b < c) {
-                first  = a;
-                second = b;
-                third = c;
-            } else if (a < c) {
-                first = a;
-                second = c;
-                third = b;
-            } else {
-                first = c;
-                second = a;
-                third = b;
-            }
-        } else {
-            if (a < c) {
-                first = b;
-                second = a;
-                third = c;
-            } else if (b < c) {
-                first = b;
-                second = c;
-                third = a;
-            } else {
-                first = c;
-                second = b;
-                third = a;
-            }
-        }
-        return new Integer[] { first, second, third };
+    private static void swap(int[] arr, int i, int j) {
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
+
+    public static void shuffle(int[] arr) {
+        for (var i = arr.length - 1; i > 0; i--) {
+            var r = ThreadLocalRandom.current().nextInt(i + 1);
+            if (i == r) continue;
+            swap(arr, i, r);
+        }
+    }
+
+    public static void quicksort(int[] nums) {
+        shuffle(nums);
+        quicksort(nums, 0, nums.length - 1);
+    }
+
+    private static void quicksort(int[] nums, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+        var partitionIndex = getPartitionIndex(nums, lo, hi);
+        quicksort(nums, lo, partitionIndex - 1);
+        quicksort(nums, partitionIndex + 1, hi);
+    }
+
+    private static int getPartitionIndex(int[] nums, int lo, int hi) {
+        var i = lo;
+        var j = hi + 1;
+
+        var partitionVal = nums[lo];
+        while (true) {
+            while (nums[++i] < partitionVal) { if (i == hi) break; }
+            while (nums[--j] > partitionVal) { if (j == lo) break; }
+            if (i >= j) {
+                break;
+            }
+            swap(nums, i, j);
+        }
+        swap(nums, lo, j);
+        return j;
+    }
+
     public List<List<Integer>> threeSum(int[] nums) {
         if (nums.length < 3) {
             return new ArrayList<>();
         }
-        var resultSet = new HashSet<List<Integer>>();
-        var allNums = new HashMap<Integer, Integer>();
-        for (var i = 0; i < nums.length; i++) {
-            var target = nums[i];
-            for (var j = 0; j < nums.length; j++) {
-                if (i == j) continue;
-                var expectedValue = 0 - (nums[j] + target);
-                var valueIndex = allNums.get(expectedValue);
-                if (valueIndex != null && j > i && !(valueIndex == j || valueIndex == i)) {
-                    var resultToAdd = Arrays.asList(sort3Values(target, nums[j], expectedValue));
-                    if (!resultSet.contains(resultToAdd)){
-                        resultSet.add(resultToAdd);
+        quicksort(nums);
+        // -4, -1, -1, 0, 1, 2
+        // -4, -3, -2, -1, -1, 0, 0, 1, 2, 3, 4
+        var resultSet = new ArrayList<List<Integer>>();
+        for (var i = 0; i < nums.length - 2 && nums[i] <= 0; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            var target = -nums[i];
+            var lo = i + 1;
+            var hi = nums.length - 1;
+
+            while (lo < hi) {
+                var sum = nums[lo] + nums[hi];
+                if (sum < target) {
+                    lo++;
+                }
+                else if (sum > target) {
+                    hi--;
+                }
+                else {
+                    final Integer first = nums[i];
+                    final Integer second = nums[lo];
+                    final Integer third = nums[hi];
+                    resultSet.add(new ArrayList<>() {{
+                        add(first);
+                        add(second);
+                        add(third);
+                    }});
+                    lo++;
+                    hi--;
+                    while (lo < hi && nums[lo] == nums[lo - 1]) {
+                        lo++;
                     }
                 }
-                allNums.put(nums[j], j);
             }
         }
-        return new ArrayList<>(resultSet);
+        return resultSet;
     }
 }
--- Problems/src/test/java/Arrays/ThreeSumTest.java
+++ Problems/src/test/java/Arrays/ThreeSumTest.java
@@ -9,13 +9,36 @@ import java.util.Arrays;
 public class ThreeSumTest {
     @Test
     public void TwoSumTest() {
-        var input = new int[]{ -1,0,1,2,-1,-4 };
         var sut = new ThreeSum();
+        var input = new int[]{ -1, 0, 1, 2, -1, -4 }; // -4, -1, -1, 0, 1, 2
         var result = sut.threeSum(input);
 
         var expectedResult = new ArrayList<ArrayList<Integer>>();
-        expectedResult.add(new ArrayList(Arrays.asList(-1, 0, 1)));
         expectedResult.add(new ArrayList(Arrays.asList(-1, -1, 2)));
+        expectedResult.add(new ArrayList(Arrays.asList(-1, 0, 1)));
         Assert.assertEquals( expectedResult, result );
+
+        input = new int[]{ 0, 0, 0, 0 };
+        result = sut.threeSum(input);
+        expectedResult = new ArrayList<ArrayList<Integer>>();
+        expectedResult.add(new ArrayList(Arrays.asList(0, 0, 0)));
+        Assert.assertEquals( expectedResult, result );
+
+        input = new int[]{ -1,0,1,2,-1,-4,-2,-3,3,0,4 };
+        result = sut.threeSum(input);
+        expectedResult = new ArrayList<ArrayList<Integer>>();
+        //[[-4,0,4],[-4,1,3],[-3,-1,4],[-3,0,3],[-3,1,2],[-2,-1,3],[-2,0,2],[-1,-1,2],[-1,0,1]]
+
+        expectedResult.add(new ArrayList(Arrays.asList(-4, 0, 4)));
+        expectedResult.add(new ArrayList(Arrays.asList(-4, 1, 3)));
+        expectedResult.add(new ArrayList(Arrays.asList(-3, -1, 4)));
+        expectedResult.add(new ArrayList(Arrays.asList(-3, 0, 3)));
+        expectedResult.add(new ArrayList(Arrays.asList(-3, 1, 2)));
+        expectedResult.add(new ArrayList(Arrays.asList(-2, -1, 3)));
+        expectedResult.add(new ArrayList(Arrays.asList(-2, 0, 2)));
+        expectedResult.add(new ArrayList(Arrays.asList(-1, -1, 2)));
+        expectedResult.add(new ArrayList(Arrays.asList(-1, 0, 1)));
+        Assert.assertEquals( expectedResult, result );
+
     }
 }
