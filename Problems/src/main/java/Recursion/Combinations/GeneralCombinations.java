c3a72cbad6267c17851dc173be45ae515fd0b91b
Grigory <GrigoryBushuev@hotmail.com>
2022-09-27 08:59:59 -0400

    Add Combinations

--- Problems/src/main/java/Recursion/Combinations/GeneralCombinations.java
+++ Problems/src/main/java/Recursion/Combinations/GeneralCombinations.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public class GeneralCombinations {
     public List<List<Character>> combine(char[] arr) {
-        return combine(new ArrayList<>(), arr, 0);
+        //return combine(new LinkedList<>(), arr, 0);
+        return getCombined(arr, 0);
     }
 
     /*
@@ -15,19 +16,39 @@ public class GeneralCombinations {
     * Run recursion for the included node
     * Collect the result for all variations
     */
-    private List<List<Character>> combine(List<Character> current, char[] arr, int index) {
+    private List<List<Character>> getCombinedWithPassedParam(List<Character> current, char[] arr, int index) {
         if (index == arr.length) {
             return new LinkedList<>() {{
                 add(current);
             }};
         }
-        var excludedResult = combine(current, arr, index + 1);
+        var excludedResult = getCombinedWithPassedParam(current, arr, index + 1);
         var toAddNode = new LinkedList<>(current);
         toAddNode.add(arr[index]);
-        var includeResult = combine(toAddNode, arr, index + 1);
+        var includeResult = getCombinedWithPassedParam(toAddNode, arr, index + 1);
         LinkedList<List<Character>> result = new LinkedList<>();
         result.addAll(excludedResult);
         result.addAll(includeResult);
         return result;
+    }
+
+    private List<List<Character>> getCombined(char[] arr, int index) {
+        if (index == arr.length) {
+            //return empty element
+            return new LinkedList<>() {{
+               add(new LinkedList<>());
+            }};
+        }
+        var combinedResults = getCombined(arr, index + 1);
+        List<List<Character>> results = new LinkedList<>();
+        for(var res : combinedResults) {
+            //add excluded
+            var toAdd = new ArrayList<>(res);
+            results.add(toAdd);
+            //create and add included
+            res.add(0, arr[index]);
+            results.add(res);
+        }
+        return results;
     }
 }
