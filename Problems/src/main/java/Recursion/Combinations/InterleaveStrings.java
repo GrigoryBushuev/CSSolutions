28eba55cacc23cc2a807eb43fadf0ef94a575bcf
Grigory <GrigoryBushuev@hotmail.com>
2022-10-30 19:25:47 -0400

    Add getInterleavingStrings

--- Problems/src/main/java/Recursion/Combinations/InterleaveStrings.java
+++ Problems/src/main/java/Recursion/Combinations/InterleaveStrings.java
@@ -3,26 +3,26 @@ package Recursion.Combinations;
 import java.util.*;
 
 public class InterleaveStrings {
-    public List<String> getInterleavingStrings(String strA, String strB) {
+    public static List<String> getInterleavingStrings(String strA, String strB) {
         LinkedList<String> result = new LinkedList<>();
         generateInterleavingStrings(strA, strB, 0, 0,strA.length() + strB.length(), new StringBuilder(), result);
         return result;
     }
 
-    private void generateInterleavingStrings(String strA, String strB, int aIndex, int bIndex, int length, StringBuilder current, LinkedList<String> result) {
+    private static void generateInterleavingStrings(String strA, String strB, int aIndex, int bIndex, int length, StringBuilder path, LinkedList<String> result) {
         if (aIndex + bIndex == length) {
-            result.add(current.toString());
+            result.add(path.toString());
             return;
         }
-        var aToAdd = new StringBuilder(current);
         if (aIndex < strA.length()) {
-            aToAdd.append(strA.charAt(aIndex));
-            generateInterleavingStrings(strA, strB, aIndex + 1, bIndex, length, aToAdd, result);
+            path.append(strA.charAt(aIndex));
+            generateInterleavingStrings(strA, strB, aIndex + 1, bIndex, length, path, result);
+            path.setLength(path.length() - 1);
         }
-        var bToAdd = new StringBuilder(current);
         if (bIndex < strB.length()) {
-            bToAdd.append(strB.charAt(bIndex));
-            generateInterleavingStrings(strA, strB, aIndex, bIndex + 1, length, bToAdd, result);
+            path.append(strB.charAt(bIndex));
+            generateInterleavingStrings(strA, strB, aIndex, bIndex + 1, length, path, result);
+            path.setLength(path.length() - 1);
         }
     }
 }
