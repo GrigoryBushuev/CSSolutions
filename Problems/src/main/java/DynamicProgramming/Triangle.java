d97b25aecce3cdd1b3bb8fecc291e1ba9dc0a8a1
Grigory <GrigoryBushuev@hotmail.com>
2023-01-01 13:32:44 -0500

    Add triangle

--- Problems/src/main/java/DynamicProgramming/Triangle.java
+++ Problems/src/main/java/DynamicProgramming/Triangle.java
@@ -1,2 +1,106 @@
-package DynamicProgramming;public class Triangle {
+package DynamicProgramming;
+
+import java.util.*;
+
+/*
+120. Triangle
+Given a triangle array, return the minimum path sum from top to bottom.
+For each step, you may move to an adjacent number of the row below.
+More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
+Example 1:
+Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+Output: 11
+Explanation: The triangle looks like:
+   2
+  3 4
+ 6 5 7
+4 1 8 3
+The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
+
+Example 2:
+Input: triangle = [[-10]]
+Output: -10
+
+Constraints:
+
+1 <= triangle.length <= 200
+triangle[0].length == 1
+triangle[i].length == triangle[i - 1].length + 1
+-104 <= triangle[i][j] <= 104
+
+Follow up: Could you do this using only O(n) extra space, where n is the total number of rows in the triangle?*/
+public class Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle.size() == 1) {
+            return triangle.get(0).get(0);
+        }
+        var bottomIndex = triangle.size();
+        var cache = new Integer[bottomIndex][triangle.get(bottomIndex - 1).size()];
+        return getMinimalTotalBottomUp(triangle);
+    }
+
+    private static int getMinimalTotalBottomUpInPlace(List<List<Integer>> trianlge) {
+        for (var row = 1; row < trianlge.size(); row++) {
+            var currentRowSize = trianlge.get(row).size();
+            for (var clmn = 0; clmn < currentRowSize; clmn++) {
+                var prevMin = Integer.MAX_VALUE;
+                var prevRow = trianlge.get(row - 1);
+                if (clmn > 0) {
+                    var topLeft = prevRow.get(clmn - 1);
+                    prevMin = clmn < currentRowSize - 1 ? Math.min(topLeft, prevRow.get(clmn)) : topLeft;
+                } else {
+                    prevMin = prevRow.get(0);
+                }
+                //Set new values as currentValue + min on the prev row
+                trianlge.get(row).set(clmn, trianlge.get(row).get(clmn) + prevMin);
+            }
+        }
+        //The result is total min of the last precalculated row
+        return Collections.min(trianlge.get(trianlge.size() - 1));
+    }
+
+    private static int getMinimalTotalBottomUp(List<List<Integer>> triangle) {
+        var cache = new int[triangle.size()];
+        Arrays.fill(cache, Integer.MAX_VALUE);
+        cache[0] = triangle.get(0).get(0);
+
+        for (var row = 1; row < triangle.size(); row++) {
+            var currentRowSize = triangle.get(row).size();
+            var prevMin = Integer.MIN_VALUE;
+            for (var clmn = 0; clmn < currentRowSize; clmn++) {
+                if (clmn > 0) {
+                    if (clmn < currentRowSize - 1) {
+                        var min = Math.min(prevMin, cache[clmn]);
+                        prevMin = cache[clmn];
+                        cache[clmn] = triangle.get(row).get(clmn) + min;
+                    } else {
+                        cache[clmn] = triangle.get(row).get(clmn) + prevMin;
+                    }
+                } else {
+                    prevMin = cache[0];
+                    cache[0] = triangle.get(row).get(clmn) + cache[0];
+                }
+            }
+        }
+        //The result is total min of the last precalculated row
+        return Arrays.stream(cache).min().getAsInt();
+    }
+
+    private static int getMinimumTotalTopDown(List<List<Integer>> triangle, int rowIndex, int columnIndex, Integer[][] cache) {
+        var cachedValue = cache[rowIndex][columnIndex];
+        if (cachedValue != null) {
+            return cachedValue;
+        }
+        var curRow = triangle.get(rowIndex);
+        var currentTotal = curRow.get(columnIndex);
+        if (rowIndex == triangle.size() - 1) {
+            return curRow.get(columnIndex);
+        }
+        var left = getMinimumTotalTopDown(triangle, rowIndex + 1, columnIndex, cache);
+        var right = getMinimumTotalTopDown(triangle, rowIndex + 1, columnIndex + 1, cache);
+        var result = currentTotal + Math.min(left, right);
+        cache[rowIndex][columnIndex] = result;
+        return result;
+    }
 }
--- Problems/src/test/java/DynamicProgramming/TriangleTest.java
+++ Problems/src/test/java/DynamicProgramming/TriangleTest.java
@@ -1,6 +1,74 @@
 package DynamicProgramming;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
 class TriangleTest {
-  
+    @Test
+    void minimumTotalTest() {
+        var sut = new Triangle();
+        var triangle1 = new LinkedList<List<Integer>>()
+        {{
+            add(new LinkedList<>() {{ add(2); }});
+            add(new LinkedList<>() {{ add(3); add(4); }});
+            add(new LinkedList<>() {{ add(5); add(6); add(7); }});
+            add(new LinkedList<>() {{ add(4); add(1); add(8); add(3); }});
+        }};
+        Assert.assertEquals(11, sut.minimumTotal(triangle1));
+        var triangle2 = new LinkedList<List<Integer>>()
+        {{
+            add(new LinkedList<>() {{ add(-10); }});
+        }};
+        Assert.assertEquals(-10, sut.minimumTotal(triangle2));
+
+        //[
+        //  [-1],
+        //  [2, 3],
+        //  [1, -1, -3]
+        //]
+        var triangle3 = new LinkedList<List<Integer>>()
+        {{
+            add(new LinkedList<>() {{ add(-1); }});
+            add(new LinkedList<>() {{ add(2); add(3); }});
+            add(new LinkedList<>() {{ add(1); add(-1); add(-3); }});
+        }};
+        Assert.assertEquals(-1, sut.minimumTotal(triangle3));
+
+        /*
+        * [
+            [-7],
+            [-2, 1],
+            [-5,-5, 9],
+            [-4,-5, 4, 4],
+            [-6,-6, 2,-1,-5],
+            [ 3, 7, 8,-3, 7,-9],
+            [-9,-1,-9, 6, 9, 0, 7],
+            [-7, 0,-6,-8, 7, 1,-4, 9],
+            [-3, 2,-6,-9,-7,-6,-9, 4, 0],
+            [-8,-6,-3,-9,-2,-6, 7,-5, 0, 7],
+            [-9,-1,-2, 4,-2, 4, 4,-1, 2,-5, 5],
+            [ 1, 1,-6, 1,-2,-4, 4,-2, 6,-6, 0, 6],
+            [-3,-3,-6,-2,-6,-2, 7,-9,-5,-7,-5, 5, 1]
+        ]*/
+        var triangle4 = new LinkedList<List<Integer>>()
+        {{
+            add(new LinkedList<>() {{ add(-7); }});
+            add(new LinkedList<>() {{ add(-2); add(1); }});
+            add(new LinkedList<>() {{ add(-5); add(-5); add(9); }});
+            add(new LinkedList<>() {{ add(-4); add(-5); add(4); add(4); }});
+            add(new LinkedList<>() {{ add(-6); add(-6); add(2); add(-1); add(-5); }});
+            add(new LinkedList<>() {{ add(3); add(7); add(8); add(-3); add(7); add(-9); }});
+            add(new LinkedList<>() {{ add(-9); add(-1); add(-9); add(6); add(9); add(0); add(7); }});
+            add(new LinkedList<>() {{ add(-7); add(0); add(-6); add(-8); add(7); add(1); add(-4); add(9); }});
+            add(new LinkedList<>() {{ add(-3); add(2); add(-6); add(-9); add(-7); add(-6); add(-9); add(4); add(0); }});
+            add(new LinkedList<>() {{ add(-8); add(-6); add(-3); add(-9); add(-2); add(-6); add(7); add(-5); add(0); add(7); }});
+            add(new LinkedList<>() {{ add(-9); add(-1); add(-2); add(4); add(-2); add(4); add(4); add(-1); add(2); add(-5); add(5); }});
+            add(new LinkedList<>() {{ add(1); add(1); add(-6); add(1); add(-2); add(-4); add(4); add(-2); add(6); add(-6); add(0); add(6); }});
+            add(new LinkedList<>() {{ add(-3); add(-3); add(-6); add(-2); add(-6); add(-2); add(7); add(-9); add(-5); add(-7); add(-5); add(5); add(1); }});
+        }};
+        Assert.assertEquals(-63, sut.minimumTotal(triangle4));
+    }
 }
